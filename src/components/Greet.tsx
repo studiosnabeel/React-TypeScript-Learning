9059ca235977b64f035ba6049d28a052d14f0416
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-25 00:23:11 +0500

    Add type string for name prop

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,4 +1,8 @@
-const Greet = (props) => {
+type GreetProps = {
+  name: string;
+};
+
+const Greet = (props: GreetProps) => {
   return (
     <div>
       <h2>Welcome {props.name}! You have 10 unread messages</h2>
