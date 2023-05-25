94493095a23f9fb1a0f388b5b2896b3e2f3eb013
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-25 11:38:09 +0500

    Import and envoke the Person Component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Greet from './components/Greet';
+import Person from './components/Person';
 
 function App() {
+  const personName = {
+    first: 'Nabeel',
+    last: 'Akbar',
+  };
   return (
     <div className="App">
-      <Greet name ="Nabeel" />
+      <Greet name="Akbar" messageCount={30} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
