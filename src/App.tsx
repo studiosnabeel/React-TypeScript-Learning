bc1afd3d7bea2f8ad6fe5c4c041e1b8c60993119
Nabeel Akbar <studiosnabeel@gmail.com>
2023-05-25 12:23:40 +0500

    Import and envoke personlist in app.tsx pass prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
 import './App.css';
 import Greet from './components/Greet';
 import Person from './components/Person';
+import { PersonList } from './components/PersonList';
 
 function App() {
   const personName = {
     first: 'Nabeel',
     last: 'Akbar',
   };
+
+  const nameList = [
+    {
+      first: 'Nabeel',
+      last: 'Akbar',
+    },
+    {
+      first: 'Fuad',
+      last: 'Nabiyev',
+    },
+    {
+      first: 'Robert',
+      last: 'Alabi',
+    },
+  ];
+
   return (
     <div className="App">
       <Greet name="Akbar" messageCount={30} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
 }
